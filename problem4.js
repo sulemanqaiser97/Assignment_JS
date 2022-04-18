bee1f7a44a1eca82c4bc1329eec374460e26ffd7
Sulemanqaiser <sulemanqaiser2@gmail.com>
2022-04-18 13:57:49 +0000

    memoizationm to increase performance of function

---
+++ problem4.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+
+let memoizedData = {};
+
+// implementation using Promises
+let memoizedFetch = function(element){
+
+    if (memoizedData[element]){
+        return new Promise((resolve)=>{
+            resolve(memoizedData[element])
+        })
+    }else{
+
+        return fetch(element)
+        .then(response => {
+            memoizedData[element] = response;
+            return response;
+        })
+        .catch(err => {return ("Unable to fetch " + element + " " + err)});
+    }
+
+}
+
+// implementation using Aysnc await
+
+let memoizedFetchASync = async function(element){
+
+    if (memoizedData[element]){
+        return memoizedData[element]
+    }else{
+        let response = await fetch(element);
+        if (response.ok){
+            memoizedData[element]= response;
+            return response
+        }
+        return "Unable to Fetch" + element;        
+
+        
+
+     
+    }
+}
+
+memoizedFetch("https://www.google.com").then(response=> console.log(response))
+
+memoizedFetchASync("https://www.google.com").then(response => console.log(response))
